71b8d1c85d136e5eeb991e28a1e07f2b6a7e83f2
Carlos Fortunato <carlosfortunatodev@gmail.com>
2024-03-08 01:36:44 -0300

    Criando função para sortear jogada do computador e verificando as escolhas para obter o ganhador

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,55 @@
-document.querySelector('#jogador-escolha-1').onclick = () =>{}
-document.querySelector('#jogador-escolha-2').onclick = () =>{}
-document.querySelector('#jogador-escolha-3').onclick = () =>{}
+let jogadorNome;
+let computadorEscolha;
+let jogadorEscolha;
 
-let jogadorNome = prompt('Qual é o seu nome?')
-document.querySelector('#jogador-nome').innerHTML = jogadorNome
+const sortear = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-document.querySelector('#mensagem').innerHTML = `Bem vindo ${jogadorNome}, está preparado? Escolha uma opção acima`
+//Verifica quem ganhou:  0)Empate  1)Jogador  2)Computador
+const verificarEscolha = (jogador, computador) => {
+    if (jogador === 1 && computador === 1) {
+        return 0;
+    } else if (jogador === 1 && computador === 2) {
+        return 2
+    } else if (jogador === 1 && computador === 3) {
+        return 1
+    } else if (jogador === 2 && computador === 1) {
+        return 1
+    } else if (jogador === 2 && computador === 2) {
+        return 0
+    } else if (jogador === 2 && computador === 3) {
+        return 2
+    } else if (jogador === 3 && computador === 1) {
+        return 2
+    } else if (jogador === 3 && computador === 2) {
+        return 1
+    } else if (jogador === 3 && computador === 3) {
+        return 0
+    } 
+}
+
+// Jogada do usuário:  1)Pedra  2)Papel  3)Tesoura
+const jogar = (escolha) => {
+    jogadorEscolha = escolha
+
+    //Sortear jogadada do computador
+    computadorEscolha = sortear(1,3)
+    let ganhador = verificarEscolha(jogadorEscolha, computadorEscolha)
+
+    
+    //Validar quem ganhou
+
+    //Somar ponto
+
+    //Exibir para o usuário
+}
+
+document.querySelector('#jogador-escolha-1').onclick = () => {jogar(1)}
+document.querySelector('#jogador-escolha-2').onclick = () => {jogar(2)}
+document.querySelector('#jogador-escolha-3').onclick = () => {jogar(3)}
+
+// jogadorNome = prompt('Qual é o seu nome?')
+// document.querySelector('#jogador-nome').innerHTML = jogadorNome
+
+// document.querySelector('#mensagem').innerHTML = `Bem vindo ${jogadorNome}, está preparado? Escolha uma opção acima`
